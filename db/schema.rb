207df743a9f9535f5eecd6f1df1d1bbc12a4c8fc
jecode93 <jeanemmanuelcadet@gmail.com>
2024-03-22 22:44:15 -0400

    Completed Iteration D1 - Finding a Cart.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_21_221301) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_23_023201) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
